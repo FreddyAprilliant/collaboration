c741a3f60f34112a04aedfbf92bebe5776e0357d
FreddyAprilliant <gygaman2@gmail.com>
2017-05-13 11:09:23 +0700

    test edit branch

--- main.py
+++ main.py
@@ -113,3 +113,5 @@ for i in range(n):
 
 with open(os.path.join("unbot", "usernamescreated.txt"), "w") as fyle:
     fyle.write("\n".join(USERNAMES_CREATED))  # write file with created users
+
+#nambah seperlunya
